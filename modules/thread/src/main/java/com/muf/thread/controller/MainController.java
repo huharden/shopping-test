b59212988c33ca7749d0f511b584e906db60e162
hutao <15170073501@163.com>
2019-03-23 17:17:58 +0800

    异常处理类实例编写

---
+++ modules/thread/src/main/java/com/muf/thread/controller/MainController.java
@@ -0,0 +1,25 @@
+package com.muf.thread.controller;
+
+import com.muf.thread.test.Station;
+
+import java.util.concurrent.Executor;
+
+/**
+ * Description:
+ *
+ * @author: hutao
+ * Date: 2019-03-22-14:44
+ */
+public class MainController {
+
+    public static void main(String[] args){
+        Executor executor = new Executor() {
+            @Override
+            public void execute(Runnable command) {
+
+            }
+        };
+
+    }
+
+}
---
+++ modules/thread/src/main/java/com/muf/thread/test/MyCallable.java
@@ -0,0 +1,13 @@
+package com.muf.thread.test;
+
+import java.util.concurrent.Callable;
+
+/**
+ * Description:
+ *
+ * @author: hutao
+ * Date: 2019-03-23-9:41
+ */
+public class MyCallable {
+
+}
